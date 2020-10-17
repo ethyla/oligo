672876c4827658e1b5ca23b8abda3c36329c8554
ethyla <ethyla@disroot.org>
2020-10-17 18:00:15 +0200

    add vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import {preset} from "vue-cli-plugin-vuetify-preset-crane/preset";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({preset});
