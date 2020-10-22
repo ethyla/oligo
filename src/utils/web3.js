566df2df88e27d6897dbe84b688d35fde4b0dbcd
ethyla <ethyla@disroot.org>
2020-10-22 23:08:04 +0200

    Inital contract connection

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -1,27 +1,10 @@
-// import {providers} from "ethers";
-// import WalletConnectProvider from "@walletconnect/web3-provider";
+import Web3 from "web3";
 
-// //  Create WalletConnect Provider
-// const web3Provider = new WalletConnectProvider({
-//   infuraId: "27e484dcd9e3efcfd25a83a78777cdf1", // Required
-// });
-// let provider;
-// export async function askForWallet() {
-//   //  Enable session (triggers QR Code modal)
-//   try {
-//     await web3Provider.enable();
-//   } catch (error) {
-//     console.log(error);
-//     return;
-//   }
+import VoteBuy from "../../truffle/build/contracts/VoteBuy";
+import VoteVault from "../../truffle/build/contracts/VoteVault";
+import Uni from "../../truffle/build/contracts/Uni";
+import GovernorAlpha from "../../truffle/build/contracts/GovernorAlpha";
 
-//   //  Wrap with Web3Provider from ethers.js
-//   provider = new providers.Web3Provider(web3Provider);
-// }
-
-// export function test() {
-//   console.log(provider.accounts);
-// }
 export async function askForWallet() {
   // eslint-disable-next-line no-undef
   return ethereum.request({method: "eth_requestAccounts"});
@@ -42,4 +25,117 @@ export async function sendTx(to, value, gasPrice, gas, data, account) {
       },
     ],
   });
+}
+
+let contracts = {
+  uni: null,
+  uniGoverner: null,
+  compound: null,
+  compoundGoverner: null,
+  votebuy: null,
+  votevault: null,
+};
+
+let web3;
+
+export async function setupWeb3() {
+  web3 = new Web3(
+    "wss://goerli.infura.io/ws/v3/ace33a466d4a46a0aaa8736a01fd08c1"
+  );
+}
+
+export async function setupContracts() {
+  contracts.votebuy = new web3.eth.Contract(
+    VoteBuy.abi,
+    VoteBuy.networks[1603392624927].address
+  );
+  contracts.votevault = new web3.eth.Contract(
+    VoteVault.abi,
+    VoteVault.networks[1603392624927].address
+  );
+  contracts.uni = new web3.eth.Contract(
+    Uni.abi,
+    Uni.networks[1603392624927].address
+  );
+  contracts.uniGoverner = new web3.eth.Contract(
+    GovernorAlpha.abi,
+    GovernorAlpha.networks[1603392624927].address
+  );
+}
+
+export async function getVaultContractData(address) {
+  let data = {
+    userCurrent: await contracts.votevault.methods.balances(address).call(),
+    userExpected: await contracts.votevault.methods.balances(address).call(),
+  };
+  return data;
+}
+
+export async function getBuyContractData(address) {
+  let userdata = await contracts.votevault.methods.balances(address).call();
+  let data = {
+    buyFor: await contracts.votebuy.methods.currentForPool().call(),
+    buyAgainst: await contracts.votevault.methods.currentAgainstPool().call(),
+    userIsFor: userdata.aggrement,
+    userOldDeposit: userdata.totalBalance - userdata.lastBalance,
+    userCurrentDeposit: userdata.lastBalance,
+  };
+  return data;
+}
+
+export async function getGovernanceTokenContractData(name) {
+  console.log(name);
+
+  let data = {
+    totalCurrent: await contracts.uni.methods
+      .balanceOf(contracts.votevault.address)
+      .call(),
+    totalPool: await contracts.uni.methods
+      .balanceOf(contracts.votevault.address)
+      .call(),
+    totalExpected: await contracts.uni.methods
+      .balanceOf(contracts.votevault.address)
+      .call(),
+    totalSupply: await contracts.uni.methods.totalSupply().call(),
+  };
+  return data;
+}
+
+export async function getProposalData(name) {
+  console.log(name);
+
+  // proposal: {
+  //   12: {
+  //     votesFor: 8000,
+  //     votesAgainst: 5000,
+  //     proposer: "0x12353467474903",
+  //     eta: 10000,
+  //     startBlock: 9000,
+  //     endBlock: 10000,
+  //   },
+  // }
+}
+
+export async function depositVotes(name, amount) {
+  console.log(name, amount);
+}
+
+export async function withdrawVotes(name) {
+  console.log(name);
+}
+
+export async function vote(name) {
+  console.log(name);
+}
+
+export async function buyVotes(name, amount, forVote) {
+  console.log(name, amount, forVote);
+}
+
+export async function withdrawBuy(name) {
+  console.log(name);
+}
+
+export async function withdrawPool(name) {
+  console.log(name);
 }
